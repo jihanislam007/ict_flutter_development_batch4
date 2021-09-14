171396889cbebf3a94b303fdab7c03f54f4e8b6a
Durbin <jihanislam007@gmail.com>
2021-09-14 14:38:40 +0600

    class -5

--- lib/main.dart
+++ lib/main.dart
@@ -30,15 +30,7 @@ class jomshedAli extends StatelessWidget {
                   child: Padding(
                     padding: const EdgeInsets.only(left: 20,right: 30),
                     child: Text(
-                      "Bangladesh, to the east of India on the Bay of "
-                          "Bengal, is a South Asian country marked by "
-                          "lush greenery and many waterways. Its "
-                          "Padma (Ganges), Meghna and Jamuna rivers "
-                          "create fertile plains, and travel by boat "
-                          "is common. On the southern coast, the "
-                          "Sundarbans, an enormous mangrove forest "
-                          "shared with Eastern India, is home to the "
-                          "royal Bengal tiger.",
+                      "Bangladesh, to the east of India on the Bay of ",
                       style: TextStyle(
                           fontSize: 24,
                           color: Colors.deepOrangeAccent,
